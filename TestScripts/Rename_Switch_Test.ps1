867293e54cfb36e80ac5771ed6e800f38be0c1a2
Octihex <Octihex@gmail.com>
2022-10-06 22:27:39 +0200

    Release 1.0 - Added Test Scripts

---
+++ TestScripts/Rename_Switch_Test.ps1
@@ -0,0 +1,18 @@
+﻿$newNamePc = Read-Host -Prompt "Nouveau nom de l'ordinateur"
+
+$ArrLaptops = "Libla", "Libol"
+$ArrDesktops = "Libde", "Libod"
+
+switch ($newNamePc.Substring(0,5)) 
+{
+    {$ArrLaptops -eq $_} 
+    { 
+        "You entered Lap." 
+    }
+
+    {$ArrDesktops -eq $_} 
+    { 
+        "You entered Des." 
+    }
+    Default { "You didn't enter PC."}
+}
