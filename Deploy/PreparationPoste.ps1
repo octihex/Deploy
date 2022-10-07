13e0a10bca0af600b02a55483adf49c502e5d90c
Octihex <octihex@gmail.com>
2022-10-07 11:55:42 +0200

    Fixed PrepareInstall.ps1

--- Deploy/PreparationPoste.ps1
+++ Deploy/PreparationPoste.ps1
@@ -14,53 +14,6 @@ if ($testadmin -eq $false)
         New-Item -ItemType Directory -Path $DeployPath | Out-Null
     }
 
-    if (!(Test-Path -PathType leaf -Path "C:\Deploy\Manufacturer.txt")) 
-    {
-        foreach ($line in (Get-CimInstance -ClassName Win32_ComputerSystem).manufacturer)
-        {
-            $Manufacturer = ($line.Split(" "))[0]
-        }
-
-        if (Select-String -InputObject $Manufacturer -Pattern Lenovo) 
-        {   
-            Out-File -FilePath $DeployPath\Manufacturer.txt -Force -InputObject Lenovo | Out-Null
-        }
-
-        elseif (Select-String -InputObject $Manufacturer -Pattern Dell) 
-        {
-            Out-File -FilePath $DeployPath\Manufacturer.txt -Force -InputObject Dell | Out-Null
-        }
-
-        else 
-        {
-            Write-Host -Object "Ce script n'est pas prévu pour cette marque d'appareil, le script va se fermer."
-            Pause
-            exit
-        }
-    }
-
-    foreach ($line in (Get-CimInstance -ClassName Win32_ComputerSystem).manufacturer)
-    {
-        $Manufacturer = ($line.Split(" "))[0]
-    }
-
-    if (Select-String -InputObject $Manufacturer -Pattern Lenovo) 
-    {   
-        Out-File -FilePath $DeployPath\Manufacturer.txt -Force -InputObject Lenovo | Out-Null
-    }
-
-    elseif (Select-String -InputObject $Manufacturer -Pattern Dell) 
-    {
-        Out-File -FilePath $DeployPath\Manufacturer.txt -Force -InputObject Dell | Out-Null
-    }
-
-    else 
-    {
-        Write-Host -Object "Ce script n'est pas prévu pour cette marque d'appareil, le script va se fermer."
-        Pause
-        exit
-    }
-
     Out-File -FilePath $DeployPath\Get_USB_Path.txt -Force -InputObject (Get-Location).Path | Out-Null
     Start-Process powershell.exe -ExecutionPolicy Bypass -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition)) 
     exit $LASTEXITCODE
