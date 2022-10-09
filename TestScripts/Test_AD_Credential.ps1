5d5a014350f9376e3d1704c822e7ea8d2b3a4a2c
Octihex <Octihex@gmail.com>
2022-10-09 22:19:16 +0200

    gty

--- TestScripts/Test_AD_Credential.ps1
+++ TestScripts/Test_AD_Credential.ps1
@@ -60,3 +60,43 @@ New-PSDrive -Name "K" -PSProvider "FileSystem" -Root "\\FS11LIB1FR\Logiciels" -P
 Test-Path -PathType leaf -Path "\\DESKTOP-2RI7D9E\Desktop\Cemu.lnk" -ErrorAction SilentlyContinue
 #Ou
 test-path -PathType container K:\
+
+#------------------------------------------------------------------------------------------------------------------
+
+function Test-ADAuthentication {
+    Param(
+        [Parameter(Mandatory)]
+        [string]$User,
+        [Parameter(Mandatory)]
+        $Password,
+        [Parameter(Mandatory = $false)]
+        $Server,
+        [Parameter(Mandatory = $false)]
+        [string]$Domain = $env:USERDOMAIN
+    )
+  
+    Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+    
+    $contextType = [System.DirectoryServices.AccountManagement.ContextType]::Domain
+    
+    $argumentList = New-Object -TypeName "System.Collections.ArrayList"
+    $null = $argumentList.Add($contextType)
+    $null = $argumentList.Add($Domain)
+    if($null -ne $Server){
+        $argumentList.Add($Server)
+    }
+    
+    $principalContext = New-Object System.DirectoryServices.AccountManagement.PrincipalContext -ArgumentList $argumentList -ErrorAction SilentlyContinue
+    if ($null -eq $principalContext) {
+        Write-Warning "$Domain\$User - AD Authentication failed"
+    }
+    
+    if ($principalContext.ValidateCredentials($User, $Password)) {
+        Write-Host -ForegroundColor green "$Domain\$User - AD Authentication OK"
+    }
+    else {
+        Write-Warning "$Domain\$User - AD Authentication failed"
+    }
+}
+#Test-ADAuthentication -User toto -Password passXX
+#Test-ADAuthentication -User toto -Password passXX -Server xxx.domain.com
