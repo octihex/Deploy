4bbd127a94b0afa73ebb5c17036c6a8bf9a767c8
Octihex <octihex@gmail.com>
2022-10-21 16:50:01 +0200

    Optimizer

--- Current TestScripts/Optimizer.ps1
+++ Current TestScripts/Optimizer.ps1
@@ -1,60 +1,61 @@
-If (!((New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))) 
+﻿If (!((New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))) 
 {
-    Start-Process Powershell.exe -Verb RunAs -ArgumentList ('-NoProfile -ExecutionPolicy Unrestricted -NoExit -File "{0}" -Elevated' -F ($Myinvocation.MyCommand.Definition))
+    Start-Process Powershell.exe -Verb RunAs -ArgumentList ('-NoProfile -ExecutionPolicy Unrestricted -NoExit -File "{0}" -Elevated' -F ($Myinvocation.MyCommand.Definition)) -WindowStyle Maximized
     Exit $LASTEXITCODE
 }
 
+Clear-Host
+Write-Output "Détection automatique de Dell Optimizer en cours"
 $Optimizer = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "Dell Optimizer"}
+
+Clear-Host
+Write-Host "Détection automatique de Dell Optimizer en cours."
 $OptimizerUI = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "DellOptimizerUI"}
+
+Clear-Host
+Write-Host "Détection automatique de Dell Optimizer en cours.."
 $Digital = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "Dell Digital Delivery Services"}
+
+Clear-Host
+Write-Host "Détection automatique de Dell Optimizer en cours..."
 $OptimizerPath = "C:\Program Files (x86)\InstallShield Installation Information\{286A9ADE-A581-43E8-AA85-6F5D58C7DC88}"
 
 If ($Optimizer)
 {
-    Write-Host "Stopping Dell Optimizer Service and Removing. . ."
+    Write-Host "Fermeture des services Dell Optimizer en cours."
     Stop-Service -Name "Dell Optimizer" -Force -PassThru
-    Sc.exe delete DellOptimizer
-    Write-Host "Uninstalling Dell Optimizer. . ."
+    Sc.exe Delete DellOptimizer
+    Write-Host "Désinstallation de Dell Optimizer en cours."
     $Optimizer.Uninstall()
     Clear-Host
 }
 
 If ($OptimizerUI)
 {
-    Write-Host "Uninstalling Dell Optimzer UI. . . "
+    Write-Host "Désinstallation de Dell Optimzer UI en cours."
     $OptimizerUI.Uninstall()
     Clear-Host
 }
 
 If ($Digital) 
 {
-    Write-Host "Uninstalling Dell Digital Delivery Services. . ."
+    Write-Host "Désinstallation des Services de Dell Optimzer."
     $Digital.Uninstall()
     Clear-Host
 }
 
 
-
-If (test-path -path "$OptimizerPath\DellOptimizer.exe" ) 
+If (Test-Path -Path "$OptimizerPath\DellOptimizer.exe") 
 {
     Start-Process "$OptimizerPath\DellOptimizer.exe" -ArgumentList "-silent -remove -runfromtemp" -NoNewWindow -Wait
+    Remove-Item $OptimizerPath -Force -Recurse -ErrorAction SilentlyContinue
     Clear-Host
-    Write-Host "Dell Optimizer Services a ete suprime"
+    Write-Host "La version universelle de Dell Optimzer a été désinstaller."
 }
 
-If (!$Optimizer) 
+If (!$Optimizer -Or !$OptimizerUI -Or !$Digital) 
 {
-    Write-Host "Dell Optimizer is NOT installed."
-}
-
-If (!$Digital) 
-{
-    Write-Host "Dell Digital Delivery Services is NOT installed."
-}
-
-If (!$OptimizerUI) 
-{
-    Write-Host "Dell Optimizer UI is NOT installed."
+    Write-Host "Dell Optimizer n'est pas installée."
 }
 
 Pause
---
+++ Current TestScripts/Optimizer_Disable.ps1
@@ -0,0 +1,28 @@
+﻿If (!((New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))) 
+{
+    Start-Process powershell.exe -Verb RunAs -ArgumentList ('-NoProfile -ExecutionPolicy Unrestricted -NoExit -File "{0}" -Elevated' -F ($Myinvocation.MyCommand.Definition)) -WindowStyle Maximized
+    Exit $LASTEXITCODE
+}
+
+If (!(Test-Path -PathType leaf -Path "C:\Program Files\Dell\DellOptimizer\DO-cli.exe"))
+{
+    "La detection automatique de Dell Optimizer a echoue."
+    "Le script va se ferme."
+    Pause
+    Exit
+}
+
+Set-Location "C:\Program Files\Dell\DellOptimizer"
+New-Item -ItemType "File" -Path ".\DO_Uninstall.log" -Force | Out-Null
+
+If ((.\do-cli.exe /Get -Name=Network.State | Select-String -Pattern "Value:").Line.Split(" ")[-1] -eq "True") 
+{
+    .\do-cli.exe /configure -name=Network.State -value=False | Out-Null
+    Out-File -FilePath ".\DO_Uninstall.log" -Append -Force -InputObject "L'option reseaux de Dell Optimizer viens d'etre desactiver." | Out-Null
+}
+
+If ((.\do-cli.exe /Get -Name=AppPerformance.State | Select-String -Pattern "Value:").Line.Split(" ")[-1] -eq "True") 
+{
+    .\do-cli.exe /configure -name=AppPerformance.State -value=False | Out-Null
+    Out-File -FilePath ".\DO_Uninstall.log" -Append -Force -InputObject "L'option applications de Dell Optimizer viens d'etre desactiver." | Out-Null
+}
