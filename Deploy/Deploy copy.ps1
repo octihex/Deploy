f6b2603b8e18bf494364bc820134e9da2284d0f8
Octihex <octihex@gmail.com>
2022-10-26 15:58:14 +0200

    Push 26/10/22

--- Deploy/Deploy copy.ps1
+++ Deploy/Deploy copy.ps1
@@ -6,7 +6,6 @@ If (!((New-Object Security.Principal.WindowsPrincipal $([Security.Principal.Wind
     Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
     Exit $LASTEXITCODE
 }
-
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 If (!(Get-Content -Path $DeployPath\Check-Install.txt -ErrorAction SilentlyContinue))
